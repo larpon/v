d6cdb090ab4fe246da02b5ab6a93be53fb7ab47b
lmp <dev.larpon@gmail.com>
2020-12-14 09:03:38 +0100

    missdoc: add support for C.* usage in pure .v files

--- cmd/tools/missdoc.v
+++ cmd/tools/missdoc.v
@@ -10,18 +10,32 @@ const (
 	tool_description = 'Prints all V functions in .v files under PATH/, that do not yet have documentation comments.'
 )
 
+enum Mode {
+	missing_fn_doc
+	c_in_v
+}
+
 struct UndocumentedFN {
 	line      int
 	signature string
 	tags      []string
 }
 
+// CFN represents C.*() aka. C functions (called in V code).
+struct CFN {
+	line      int
+	signature string
+}
+
 struct Options {
 	show_help    bool
 	collect_tags bool
 	deprecated   bool
+	mode         Mode
 }
 
+// collect traverses `path` recursively and collect files in `l` letting the user provide
+// the collecting logic via function argument `f`.
 fn collect(path string, mut l []string, f fn (string, mut []string)) {
 	if !os.is_dir(path) {
 		return
@@ -38,6 +52,8 @@ fn collect(path string, mut l []string, f fn (string, mut []string)) {
 	return
 }
 
+// report_undocumented_functions_in_path reports to stdout all function signatures that does not
+// have any comments attached.
 fn report_undocumented_functions_in_path(opt Options, path string) {
 	mut files := []string{}
 	collect_fn := fn (path string, mut l []string) {
@@ -98,6 +114,70 @@ fn report_undocumented_functions_in_path(opt Options, path string) {
 	}
 }
 
+// report_c_functions_in_pure_v_code reports to stdout all lines with C.*() function signatures
+// found in any *.v files.
+fn report_c_functions_in_pure_v_code(opt Options, path string) {
+	mut files := []string{}
+	collect_fn := fn (path string, mut l []string) {
+		if path.ends_with('.c.v') {
+			return
+		} else if os.file_ext(path) == '.v' {
+			l << os.real_path(path)
+		}
+	}
+	collect(path, mut files, collect_fn)
+	for f in files {
+		contents := os.read_file(f) or { panic(err) }
+		lines := contents.split('\n')
+		// Skip test files
+		//if f.ends_with('_test.v') {
+		//	continue
+		//}
+		mut results := []CFN{}
+		for i, line in lines {
+			if line.contains(' C.') {
+				if line.starts_with('//') {
+					continue
+				}
+				clean_line := line
+				results << CFN{i + 1, clean_line}
+				// println('Match: $line')
+				/*
+				if i > 0 && lines.len > 0 {
+					mut line_above := lines[i - 1]
+					if !line_above.starts_with('//') {
+						mut tags := []string{}
+						mut grab := true
+						for j := i - 1; j >= 0; j-- {
+							prev_line := lines[j]
+							if prev_line.contains('}') { // We've looked back to the above scope, stop here
+								break
+							} else if prev_line.starts_with('[') {
+								tags << collect_tags(prev_line)
+								continue
+							} else if prev_line.starts_with('//') { // Single-line comment
+								grab = false
+								break
+							}
+						}
+						if grab {
+							clean_line := line.all_before_last(' {')
+							info << UndocumentedFN{i + 1, clean_line, tags}
+						}
+					}
+				}
+				*/
+			}
+		}
+		if results.len > 0 {
+			for c_fn in results {
+				println('$f:$c_fn.line:0:$c_fn.signature')
+			}
+		}
+	}
+}
+
+// collect_tags returns a string list of all V tags found in `line`.
 fn collect_tags(line string) []string {
 	mut cleaned := line.all_before('/')
 	cleaned = cleaned.replace_each(['[', '', ']', '', ' ', ''])
@@ -119,12 +199,24 @@ fn main() {
 		show_help: fp.bool('help', `h`, false, 'Show this help text.')
 		deprecated: fp.bool('deprecated', `d`, false, 'Include deprecated functions in output.')
 		collect_tags: fp.bool('tags', `t`, false, 'Also print function tags if any is found.')
+		mode: if fp.string('mode', `m`, 'missing_doc', 'Also print function tags if any is found.') == 'missing_doc' { Mode.missing_fn_doc } else { Mode.c_in_v }
 	}
+
 	if opt.show_help {
 		println(fp.usage())
 		exit(0)
 	}
-	for path in os.args[1..] {
-		report_undocumented_functions_in_path(opt, path)
+
+	match opt.mode {
+		.missing_fn_doc {
+			for path in os.args[1..] {
+				report_undocumented_functions_in_path(opt, path)
+			}
+		}
+		.c_in_v {
+			for path in os.args[1..] {
+				report_c_functions_in_pure_v_code(opt, path)
+			}
+		}
 	}
 }
